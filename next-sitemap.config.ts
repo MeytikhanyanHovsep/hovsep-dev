87fc67ce4c93e442c89100f83633a2819d0b710e
MeytikhanyanHovsep <meytikhanyan.hovsep@gmail.com>
2026-03-28 16:38:35 +0400

    fixed deploy bug

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -1,8 +1,11 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: "https://hovsep-dev.vercel.app/",
-    generateRobotsTxt: true,
-    changefreq: "daily",
-    priority: 0.7,
-    sitemapSize: 5000,
+import type { IConfig } from "next-sitemap";
+
+const config: IConfig = {
+  siteUrl: "https://hovsep-dev.vercel.app",
+  generateRobotsTxt: true,
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 5000,
 };
+
+export default config;
